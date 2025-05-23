8fd5281b3c442739e7ffe124225755bfe0898e6a
Jimpal1811 <jimpal2004@gmail.com>
2025-05-23 11:22:20 +0530

    new all

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,11 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
-//  @type {import('tailwindcss').Config} 
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
